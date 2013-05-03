5771c6fd570cc014afaefd276c3ffe8fcc847022
gsamek <geoff.samek@yahoo.com>
2013-05-03 11:07:52 -0700

    Finished

--- spec/elevator.rb
+++ spec/elevator.rb
@@ -1,2 +1,78 @@
 class Elevator
+  attr_accessor :floors, :current_floor, :request_queue, :direction
+  
+  def initialize (number_floors=10, current_floor=1)
+    @floors = 1..number_floors
+    @current_floor = current_floor
+    @request_queue = []
+    @direction = 0 # -1 for down, 0 for stopped, 1 for up
+  end
+  
+  def call_request origin_floor, direction
+    if @floors.member?(origin_floor) && (-1..1).member?(direction)
+      @request_queue.push({'type'=>'call','floor'=>origin_floor, 'direction'=>direction})
+    end
+  end
+  
+  def floor_request floor
+    if @floors.member?(floor)   
+      @request_queue.push({'type'=>'request','floor'=>floor})
+    end
+  end
+  
+  # Move elevator to next requested floor based on request queue, return true if you visited that floor
+  def move
+    visited = false
+    if @request_queue.length == 0
+      visited = false
+    else
+      if @direction == 0
+        # Set direction based on first item in queue  
+        if @current_floor < @request_queue[0]['floor']
+          @direction = 1
+        elsif @current_floor > @request_queue[0]['floor']
+          @direction = -1
+        else
+          # We are already on the requested floor, keep the direction 0, remove this request, we're done!
+          @request_queue.shift
+          visited = true
+        end
+      end
+      # Move to next floor in current direction unless that is a floor out of our range
+      if(@floors.member?(@current_floor + @direction))
+        @current_floor += @direction
+      else
+        # don't move floors wait for new direction
+        @direction = 0
+      end
+      # Are there any requests or calls for this floor
+      if(@request_queue.find{|req| req['floor'] == @current_floor})
+        visited = true
+        # if this was a call request, we now change direction to match the request
+        if(@request_queue.find{|req| (req['floor'] == @current_floor) && (req['type'] == 'call')})
+          @direction = @request_queue.find{|req| (req['floor'] == @current_floor) && (req['type'] == 'call')}['direction']
+        end
+        # Remove requests for this new floor
+        @request_queue.keep_if{|req| (req['floor'] != @current_floor)}
+      end
+      visited
+    end
+      
+  end
+  
+  def start_elevator
+    # track all the visited floors
+    floors_visited = []
+    # Current floor is visited by default
+    floors_visited.push(@current_floor)
+    
+    #as long as there are floors requested or elevator calls made, keep going
+    while @request_queue.length > 0 do
+      if move
+        floors_visited.push(@current_floor)
+      end
+    end
+    
+    return floors_visited
+  end
 end
--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -1,4 +1,91 @@
 require 'elevator'
 
 describe "Elevator" do
+  before (:each) do
+    @e = Elevator.new(20)
+  end
+  
+  describe "#call_request" do
+    context "out of range call request" do
+      before do
+        @e.call_request(22, 1)
+      end
+      
+      it "should still have an empty request queue" do
+        @e.request_queue.should == []
+      end
+    end
+    
+    context "in range call request" do
+      before do
+        @e.call_request(17, -1)
+      end
+      
+      it "should have a request queue length of 1" do
+        @e.request_queue.length.should == 1
+      end
+    end
+  end
+  
+  describe "#floor_request" do
+    context "out of range floor request" do
+      before do
+        @e.floor_request(31)
+      end
+      
+      it "should still have an empty request queue" do
+        @e.request_queue.should == []
+      end
+    end
+    
+    context "in range floor request" do
+      before do
+        @e.floor_request(11)
+      end
+      
+      it "should have a request queue length of 1" do
+        @e.request_queue.length.should == 1
+      end
+    end
+  end
+  
+  describe "#move" do
+    context "elevator simple move" do
+      before do
+        @e.call_request(11, -1)
+        20.times do
+          @e.move
+        end
+      end
+      
+      it "should have a current floor of 11" do
+        @e.current_floor.should == 11
+      end
+      
+      it "should have an empty request queue" do
+        @e.request_queue.length.should == 0
+      end
+    end
+  end
+  
+  describe "#move" do
+    context "elevator complete run" do
+      before do
+        @e.call_request(9, -1)
+        @e.call_request(2, 1)
+        @e.call_request(7, -1)
+        @e.floor_request(5)
+        @e.floor_request(1)
+        @visited = @e.start_elevator
+      end
+  
+      it "should visit all requested floors" do
+        @visited.should == [1,2,5,7,1,9]
+      end
+    
+      it "should have an empty request queue when done" do
+        @e.request_queue.length.should == 0
+      end
+    end
+  end
 end
